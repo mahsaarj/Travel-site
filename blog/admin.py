36b8d750fb4114fe88f2d2af2e3578792b4af57c
mahsaarj <103898788+mahsaarj@users.noreply.github.com>
2023-09-15 10:58:30 -0700

    edited admin added fields of images and other things

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    fields = ('title',)
+    fields = ('status','image','author', 'title','content', 'counted_views','published_date')
     list_display = ('title','author','counted_views','status','published_date','created_date')
     list_filter = ('status','author')
     ordering = ['-created_date']
