ef0d58757d4c02d1358c4cafa23d2a87a67a0e32
mahsaarj <103898788+mahsaarj@users.noreply.github.com>
2023-12-28 00:50:12 -0800

    added new changes in blog/templateTags

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 
 # Register your models here.
-@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    fields = ('status','image','author', 'title','content', 'counted_views','published_date')
     list_display = ('title','author','counted_views','status','published_date','created_date')
     list_filter = ('status','author')
     ordering = ['-created_date']
     search_fields = ['title','content']
+
+admin.site.register(Category)
+admin.site.register(Post,PostAdmin )
+
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path,  include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path ('url address' , 'view')
     path('', include('website.urls')),
-    path('blog/',include('blog.urls'))
+    path('blog/',include('blog.urls')),
+    path('blog/test', views.test, name='test'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
