4b0deb57b7b17980602a57c1773919ecc13a016c
mahsaarj <103898788+mahsaarj@users.noreply.github.com>
2024-02-02 00:44:44 -0800

    update test form using modelForm and add it to database

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from website.models import Contact
-from website.forms import NameForm
+from website.forms import NameForm , ContactForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -12,16 +12,12 @@ def contact_view(request):
     return render(request, 'website/contact.html')
 def test_view(request):
     if request.method == 'POST':
-        form = NameForm(request.POST)
+        form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            subject = form.cleaned_data['subject']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            print(name , email, subject , message)
+            form.save()
             return HttpResponse('DONE')
         else:
             return HttpResponse('NOT VALID')
 
-    form = NameForm()
+    form = ContactForm
     return render(request , 'test.html', {'form': form})
