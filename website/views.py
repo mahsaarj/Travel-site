9752961fe0e352e08a398d5540effa60f69c69c5
mahsaarj <103898788+mahsaarj@users.noreply.github.com>
2024-02-02 00:26:28 -0800

    added form page in test file without saving information

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from website.models import Contact
+from website.forms import NameForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -8,3 +10,18 @@ def about_view(request):
     return render(request, 'website/about.html')
 def contact_view(request):
     return render(request, 'website/contact.html')
+def test_view(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print(name , email, subject , message)
+            return HttpResponse('DONE')
+        else:
+            return HttpResponse('NOT VALID')
+
+    form = NameForm()
+    return render(request , 'test.html', {'form': form})
